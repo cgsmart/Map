209fe4ac12cc75f6ffb1830479aa44d633184faa
ChenShuai <zzuchenshuai@gmail.com>
2018-12-13 22:57:32 +0800

    test

---
+++ app/src/main/java/com/example/chenshuai/myapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.chenshuai.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(MainActivity.this,"This is a Toast!",Toast.LENGTH_SHORT).show();
+        setContentView(R.layout.activity_main);
+    }
+}
