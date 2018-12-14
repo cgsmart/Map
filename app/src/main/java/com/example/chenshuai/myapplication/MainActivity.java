a5135669e96ef720848065685bf66308bae5326b
ChenShuai <zzuchenshuai@gmail.com>
2018-12-14 09:43:22 +0800

    Commit!

--- app/src/main/java/com/example/chenshuai/myapplication/MainActivity.java
+++ app/src/main/java/com/example/chenshuai/myapplication/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.chenshuai.myapplication;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
+
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +16,6 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(MainActivity.this,"This is a Toast!",Toast.LENGTH_SHORT).show();
         Toast.makeText(MainActivity.this,"This is a Toast3!",Toast.LENGTH_SHORT).show();
         setContentView(R.layout.activity_main);
+        Uri uri = Uri.parse("content://com.example.app.provider/table1");
     }
 }
